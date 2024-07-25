b86dc1e71f6016b115c9dac3610f57fa4b6228c9
gairik <gairikgmandal@gmail.com>
2024-07-25 13:02:10 +0530

    binarySearchBasic

--- binary_search/binarySearch.cpp
+++ binary_search/binarySearch.cpp
@@ -1,7 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(vector<int> &nums, int target)
+int binarySearch(int low, int high, int target, vector<int> &nums)
+{
+    if (low > high)
+        return -1;
+    int mid = (low + high) / 2;
+    if (nums[mid] == target)
+        return mid;
+    else if (nums[mid] > target)
+        return binarySearch(low, mid - 1, target, nums);
+    else
+        return binarySearch(mid + 1, high, target, nums);
+}
+
+int binarySearch1(vector<int> &nums, int target)
 {
     int n = nums.size(); // size of the array
     int low = 0, high = n - 1;
@@ -24,7 +37,7 @@ int main()
 {
     vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
     int target = 6;
-    int ind = binarySearch(a, target);
+    int ind = binarySearch(0,7,target,a);
     if (ind == -1)
         cout << "The target is not present." << endl;
     else
